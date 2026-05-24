b48bce115dcb942170ab1f1c80b17dad117dc612
Nicolas Herschke <nicolas.herschke@gmx.de>
2026-05-24 08:29:59 +0200

    feat: deploy to custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://adnomina.github.io",
-	base: "/blog",
+	site: "https://adnomina.dev",
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
